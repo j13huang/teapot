d26fae0dc0d205ed7b5205f29371541f9b1697c3
John Huang <j13huang@gmail.com>
2019-12-30 19:05:40 -0800

    Initial Commit

---
+++ Teapot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Teapot
+//
+//  Created by John Huang on 12/30/19.
+//  Copyright © 2019 John Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
